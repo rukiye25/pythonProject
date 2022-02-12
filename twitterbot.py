01ae48f656925b78bb8ef429e587f55ecd671646
rukiye25 <77140579+rukiye25@users.noreply.github.com>
2022-02-12 13:44:39 +0300

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -23,7 +23,7 @@ sifre.send_keys(Keys.ENTER)
 time.sleep(4)
 
 sayac=1
-"""while sayac<51:
+while sayac<51:
     tweei_click=driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[1]/div[3]/a/div/span/div/div/span/span').click()
     time.sleep(4)
     tweet=driver.find_element_by_xpath('//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div')
@@ -31,7 +31,7 @@ sayac=1
     time.sleep(2)
     tweet_click=driver.find_element_by_xpath('//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span').click()
     sayac+=1
-    time.sleep(5)"""
+    time.sleep(5)
 
 while sayac<51:
     tweei_click=driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[1]/div[3]/a/div/span/div/div/span/span').click()
